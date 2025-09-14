df51a4d7dd6f35332bb470a96ae86d51aecff0d7
Enzo Tomas <tomasenzo03@gmail.com>
2025-09-14 00:13:08 -0300

    Rotacion del espejo

--- Assets/Scripts/Espejo_Animacion.cs
+++ Assets/Scripts/Espejo_Animacion.cs
@@ -3,20 +3,29 @@ using UnityEngine.InputSystem;
 
 public class ClickTransform : MonoBehaviour
 {
-    public Transform objetoAModificar; 
+    public Transform espejo1;
+    public Transform espejo2;
 
-    private Vector3 originalPosition;
-    private Quaternion originalRotation;
+    private Vector3 originalPositionEspejo1;
+    private Quaternion originalRotationEspejo1;
+    
+    private Vector3 originalPositionEspejo2;
+    private Quaternion originalRotationEspejo2;
 
     [Header("Nueva posición y rotación")]
-    public Vector3 newPosition;  
-    public Vector3 newRotation;  
+    public Vector3 newPositionEspejo1;  
+    public Vector3 newRotationEspejo1;
+
+    public Vector3 newPositionEspejo2;
+    public Vector3 newRotationEspejo2;
 
     void Start()
     {
-        
-        originalPosition = objetoAModificar.position;
-        originalRotation = objetoAModificar.rotation;
+        originalPositionEspejo1 = espejo1.localPosition;
+        originalRotationEspejo1 = espejo1.localRotation;
+
+        originalPositionEspejo2 = espejo2.localPosition;
+        originalRotationEspejo2 = espejo2.localRotation;
     }
 
     void Update()
@@ -24,15 +33,21 @@ public class ClickTransform : MonoBehaviour
         if (Mouse.current.rightButton.isPressed)
         {
             Debug.Log("Click izquierdo detectado");
-            objetoAModificar.position = newPosition;
-            objetoAModificar.rotation = Quaternion.Euler(newRotation);
+            espejo1.localPosition= newPositionEspejo1;
+            espejo1.localRotation = Quaternion.Euler(newRotationEspejo1);
+
+            espejo2.localPosition = newPositionEspejo2;
+            espejo2.localRotation = Quaternion.Euler(newRotationEspejo2);
             Debug.Log("Click izquierdo detectado");
         }
         else
         {
             Debug.Log("Click derecho detectado");
-            objetoAModificar.position = originalPosition;
-            objetoAModificar.rotation = originalRotation;
+            espejo1.localPosition = originalPositionEspejo1;
+            espejo1.localRotation = originalRotationEspejo1;
+
+            espejo2.localPosition = originalPositionEspejo2;
+            espejo2.localRotation = originalRotationEspejo2;
             Debug.Log("Click derecho detectado");
         }
     }
