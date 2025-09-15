82ad9f78d2f940e603dc3271297ccabcc9ed1551
Enzo Tomas <tomasenzo03@gmail.com>
2025-09-15 09:02:11 -0300

    Temporizador parte 2

--- Assets/Scripts/Espejo_Animacion.cs
+++ Assets/Scripts/Espejo_Animacion.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Rendering;
 
 public class UtilidadDeEspejo: MonoBehaviour
 {
@@ -15,11 +16,12 @@ public class UtilidadDeEspejo: MonoBehaviour
     //Temporizador
     public GameObject negro;
 
-    private bool activadoJumpScare;  
-    private float temporizadorEspejo = 0f;
+    private bool activadoJumpScare;
+    public float temporizadorEspejo = 0f;
     private float tiempoLimiteEspejo = 3f;
 
-    private float temporizadorJumpScare = 2f;
+    private float temporizadorJumpScare = 0f;
+    private float limiteJumpScare = 2f;
 
 
     private float couldownEspejo = 5f;
@@ -36,27 +38,38 @@ public class UtilidadDeEspejo: MonoBehaviour
     {
         if (Mouse.current.rightButton.isPressed)
         {
-            activadoJumpScare = true;
             espejo1.localPosition = newPositionEspejo1;
             espejo1.localRotation = Quaternion.Euler(newRotationEspejo1);
-
+            temporizadorEspejo += Time.deltaTime;
         }
         else
         {
             espejo1.localPosition = originalPositionEspejo1;
             espejo1.localRotation = originalRotationEspejo1;
-            activadoJumpScare = false;
+            temporizadorEspejo = 0f;
         }
 
-        if (activadoJumpScare)
+        if (temporizadorEspejo >= tiempoLimiteEspejo)
         {
+            activadoJumpScare = true;
             if (temporizadorEspejo >= tiempoLimiteEspejo)
             {
                 negro.SetActive(true);
+                temporizadorJumpScare += Time.deltaTime;
+
+            }
+
+            if (temporizadorJumpScare >= limiteJumpScare)
+            {
+                negro.SetActive(false);
             }
 
             temporizadorEspejo += Time.deltaTime;
 
+        }
+        else
+        {
+            activadoJumpScare = false;
         }
         
     }
