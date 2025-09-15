9e5fa6f33129b722c42e188b69cc598678fd8649
Enzo Tomas <tomasenzo03@gmail.com>
2025-09-14 22:26:58 -0300

    Switch entre objetos

--- Assets/Scripts/Espejo_Animacion.cs
+++ Assets/Scripts/Espejo_Animacion.cs
@@ -1,31 +1,23 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class ClickTransform : MonoBehaviour
+public class UtilidadDeEspejo: MonoBehaviour
 {
     public Transform espejo1;
-    public Transform espejo2;
 
     private Vector3 originalPositionEspejo1;
     private Quaternion originalRotationEspejo1;
-    
-    private Vector3 originalPositionEspejo2;
-    private Quaternion originalRotationEspejo2;
+
 
     [Header("Nueva posición y rotación")]
     public Vector3 newPositionEspejo1;  
     public Vector3 newRotationEspejo1;
 
-    public Vector3 newPositionEspejo2;
-    public Vector3 newRotationEspejo2;
 
     void Start()
     {
         originalPositionEspejo1 = espejo1.localPosition;
-        originalRotationEspejo1 = espejo1.localRotation;
 
-        originalPositionEspejo2 = espejo2.localPosition;
-        originalRotationEspejo2 = espejo2.localRotation;
     }
 
     void Update()
@@ -36,9 +28,6 @@ public class ClickTransform : MonoBehaviour
             espejo1.localPosition= newPositionEspejo1;
             espejo1.localRotation = Quaternion.Euler(newRotationEspejo1);
 
-            espejo2.localPosition = newPositionEspejo2;
-            espejo2.localRotation = Quaternion.Euler(newRotationEspejo2);
-            Debug.Log("Click izquierdo detectado");
         }
         else
         {
@@ -46,9 +35,6 @@ public class ClickTransform : MonoBehaviour
             espejo1.localPosition = originalPositionEspejo1;
             espejo1.localRotation = originalRotationEspejo1;
 
-            espejo2.localPosition = originalPositionEspejo2;
-            espejo2.localRotation = originalRotationEspejo2;
-            Debug.Log("Click derecho detectado");
         }
     }
 }
