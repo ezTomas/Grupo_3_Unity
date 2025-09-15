5fdbd24140e92bc907b6d86f2aacf26177f38648
Enzo Tomas <tomasenzo03@gmail.com>
2025-09-14 23:30:56 -0300

    Temporizador parte 1

--- Assets/Scripts/Espejo_Animacion.cs
+++ Assets/Scripts/Espejo_Animacion.cs
@@ -8,15 +8,27 @@ public class UtilidadDeEspejo: MonoBehaviour
     private Vector3 originalPositionEspejo1;
     private Quaternion originalRotationEspejo1;
 
-
     [Header("Nueva posición y rotación")]
-    public Vector3 newPositionEspejo1;  
+    public Vector3 newPositionEspejo1;
     public Vector3 newRotationEspejo1;
+
+    //Temporizador
+    public GameObject negro;
+
+    private bool activadoJumpScare;  
+    private float temporizadorEspejo = 0f;
+    private float tiempoLimiteEspejo = 3f;
+
+    private float temporizadorJumpScare = 2f;
+
+
+    private float couldownEspejo = 5f;
 
 
     void Start()
     {
         originalPositionEspejo1 = espejo1.localPosition;
+        negro.SetActive(false);
 
     }
 
@@ -24,17 +36,28 @@ public class UtilidadDeEspejo: MonoBehaviour
     {
         if (Mouse.current.rightButton.isPressed)
         {
-            Debug.Log("Click izquierdo detectado");
-            espejo1.localPosition= newPositionEspejo1;
+            activadoJumpScare = true;
+            espejo1.localPosition = newPositionEspejo1;
             espejo1.localRotation = Quaternion.Euler(newRotationEspejo1);
 
         }
         else
         {
-            Debug.Log("Click derecho detectado");
             espejo1.localPosition = originalPositionEspejo1;
             espejo1.localRotation = originalRotationEspejo1;
+            activadoJumpScare = false;
+        }
+
+        if (activadoJumpScare)
+        {
+            if (temporizadorEspejo >= tiempoLimiteEspejo)
+            {
+                negro.SetActive(true);
+            }
+
+            temporizadorEspejo += Time.deltaTime;
 
         }
+        
     }
 }
