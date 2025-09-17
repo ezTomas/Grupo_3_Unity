58fbd05d8e40895729bbdfe425348d52c1a0a021
Enzo Tomas <195832517+ezTomas@users.noreply.github.com>
2025-09-17 10:39:19 -0300

    Linterna

---
+++ Assets/Linterna.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Linterna : MonoBehaviour
+{
+    public Light luzLinterna;
+    public bool linternaActiva;
+
+
+    private void Update()
+    {
+        if (Mouse.current.leftButton.wasPressedThisFrame)
+        {
+            linternaActiva = !linternaActiva;
+            
+            if (linternaActiva == true)
+            {
+                luzLinterna.enabled = true;
+            }
+
+            if (linternaActiva == false) 
+            {
+                luzLinterna.enabled = false;
+            }
+        }
+
+    }
+}
+
+
+
+
+
+
