4a4070f498ffeb42e0e0e1c40a8139e1a8b89192
Ailin410 <ailinvanesa398@gmail.com>
2025-09-30 02:17:25 -0300

    Suma de los números a la interfaz

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,16 +1,39 @@
 using UnityEngine;
+using TMPro;
+using System.Collections.Generic;
 
 public class UIManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public static UIManager Instance;
+
+    [SerializeField] private TextMeshProUGUI textoNumeros;
+
+    private List<string> numerosEncontrados = new List<string>();
+
+    private void Awake()
     {
-        
+        if (Instance == null) 
+            Instance = this;
+
+        else 
+            Destroy(gameObject);
+
+        textoNumeros.text = "";
+        textoNumeros.gameObject.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void MostrarNumero(string numero)
     {
-        
+        textoNumeros.text += numero + " ";
+
+        if (!numerosEncontrados.Contains(numero))
+        {
+            numerosEncontrados.Add(numero);
+        }
+
+        textoNumeros.text = string.Join(" ", numerosEncontrados);
+
+        if (!textoNumeros.gameObject.activeSelf)
+            textoNumeros.gameObject.SetActive(true);
     }
 }
