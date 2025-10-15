62e5d1688ebbbf68f4ca89d0fde9aac492b002e0
Victor 72 <130411821+Vitor72@users.noreply.github.com>
2025-10-15 00:53:20 -0300

    Colocacion_rancho

--- Assets/Scripts/Couldown_Espejo.cs
+++ Assets/Scripts/Couldown_Espejo.cs
@@ -18,7 +18,7 @@ public class Couldown_Espejo : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        negro.SetActive(false);
+        
     }
 
     // Update is called once per frame
