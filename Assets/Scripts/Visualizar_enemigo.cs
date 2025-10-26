e00be06670b3b401f2b1e8183cfdfc7c73b35428
Victor 72 <130411821+Vitor72@users.noreply.github.com>
2025-10-25 22:58:59 -0300

    Parpadeo

--- Assets/Scripts/Visualizar_enemigo.cs
+++ Assets/Scripts/Visualizar_enemigo.cs
@@ -4,8 +4,10 @@ public class Visualizar_enemigo : MonoBehaviour
 {
     public Camera camara;
     int capa_enemy;
+    public float cronometro_cambio;
     public float cronometro;
     private int modo;
+    private int modo_cambio;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -16,27 +18,42 @@ public class Visualizar_enemigo : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
-        cronometro += 1 * Time.deltaTime;
-        if (cronometro >= 2)
+        cronometro_cambio += 1 * Time.deltaTime;
+        if (cronometro_cambio >= 10)
         {
-            modo = Random.Range(0, 2);
-            cronometro = 0;
+            modo_cambio = Random.Range(0, 2);
+            cronometro_cambio = 0;
         }
-        switch (modo)
+        switch (modo_cambio)
         {
             case 0:
-                
-                camara.cullingMask |= (1 << capa_enemy);
-                Debug.Log("Enemigo visible");
+                camara.cullingMask &= ~(1 << capa_enemy);
+                Debug.Log("Enemigo Invisible");
                 break;
             case 1:
-                camara.cullingMask &= ~(1 << capa_enemy);
-                Debug.Log("Enemigo invisible");
+                cronometro_cambio += 4;
+                cronometro += 1 * Time.deltaTime;
+                if (cronometro >= 2)
+                {
+                    //modo = Random.Range(0, 2);
+                    cronometro = 0;
+                }
+                switch (modo)
+                {
+                    case 0:
+                        camara.cullingMask |= (1 << capa_enemy);
+                        
+                        modo++;
+                        break;
+                    case 1:
+                        camara.cullingMask &= ~(1 << capa_enemy);
+                        
+                        modo--;
+                        break;
+                }
+                Debug.Log("Enemigo parpadeo");
                 break;
-
         }
-        
     }
 
 }
