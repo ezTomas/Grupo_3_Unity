1bbe42e84fb1f5e92164714c0e388e813cc3c30e
Enzo Tomas <tomasenzo03@gmail.com>
2025-11-14 22:57:10 -0300

    Menu Principal

---
+++ Assets/Menu_Principal.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu_Principal : MonoBehaviour
+{
+    
+    public void Jugar()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
